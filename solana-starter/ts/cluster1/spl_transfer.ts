15e7c50ffb48edaa7ee2967910b13db25412dacf
Jasondevweb3 <159626090+Jasondevweb3@users.noreply.github.com>
2024-02-21 18:52:15 +0100

    l2

--- solana-starter/ts/cluster1/spl_transfer.ts
+++ solana-starter/ts/cluster1/spl_transfer.ts
@@ -1,6 +1,7 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
 import wallet from "../cluster1/wallet/wba-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import bs58 from 'bs58';
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,18 +11,39 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("3hDSRkhukpXvhzUdHzprx1mVCVeqgUTyUXtF794pbesD");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("FAXDZafhXmNBykiqSQtDmT3ZELDiRifzPtq9Yv4zEuvM");
 
 (async () => {
     try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+        // // Get or create the token account for the sender (fromWallet)
+        const fromWalletTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection, 
+            keypair,
+            mint,
+            keypair.publicKey);
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+        // // Get or create the token account for the receiver (toWallet)
+        const toWalletTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey
+        );
 
-        // Transfer the new token to the "toTokenAccount" we just created
+        // // Transfer tokens from the sender to the receiver
+        const transferTx = await transfer(
+            connection,
+            keypair,
+            fromWalletTokenAccount.address,
+            toWalletTokenAccount.address,
+            keypair.publicKey,
+            1_000_000
+        );
+
+        console.log("Transfer transaction:", transferTx);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
